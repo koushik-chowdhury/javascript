958b4e7158cf8bb5812fbfb98c27d616546d4dd7
Koushik Chowdhury <csakoushik@gmail.com>
2025-07-03 20:31:11 +0530

    example of coercion, typeof checks, and truthy/falsy evaluations

---
+++ datatypes/script.js
@@ -0,0 +1,24 @@
+// output
+
+console.log(true+false)
+console.log(null+1)
+console.log(5+'5')
+console.log(!!undefined)
+console.log(!undefined)
+console.log([]+[])
+
+// types 
+
+console.log(typeof([]))
+console.log(typeof null)
+console.log(typeof 123n)
+
+// Truthy or Falsy 
+
+console.log(Boolean(0))
+console.log(Boolean('0'))
+console.log(Boolean([]))
+console.log(Boolean(undefined))
+
+console.log(5=='5')
+console.log(5==='5')
