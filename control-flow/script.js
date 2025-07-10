823ae67db5c52fbfce040764dbc1f78c04f99433
Koushik Chowdhury <csakoushik@gmail.com>
2025-07-10 13:48:55 +0530

    Add control flow examples

---
+++ control-flow/script.js
@@ -0,0 +1,32 @@
+// Grade Checker using if-else
+
+let marks = 87;
+
+if (marks >= 90) {
+  console.log("Grade: A");
+} else if (marks >= 75) {
+  console.log("Grade: B");
+} else if (marks >= 60) {
+  console.log("Grade: C");
+} else {
+  console.log("Grade: F");
+}
+
+
+// Fruit Color using switch-case
+
+let fruit = "banana";
+
+switch (fruit) {
+  case "apple":
+    console.log("Red");
+    break;
+  case "banana":
+    console.log("Yellow");
+    break;
+  case "grape":
+    console.log("Purple");
+    break;
+  default:
+    console.log("Unknown fruit");
+}
